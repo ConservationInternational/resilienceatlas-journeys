04f4c6695659b99f2e766991dc0475a5096b564e
Alex Zvoleff <azvoleff@gmail.com>
2016-11-18 17:29:27 -0500

    Finalize plots.

--- coffee-africa-cc/coffee-mintemp.R
+++ coffee-africa-cc/coffee-mintemp.R
@@ -3,7 +3,10 @@ library(dplyr)
 library(cdbr)
 library(foreach)
 library(plotly)
+library(raster)
 library(RColorBrewer)
+library(rgdal)
+library(tidyr)
 
 # Extract minimum temperatures areas where more than xx ha of Arabica is grown, 
 # and summarize trends by country
@@ -11,7 +14,8 @@ library(RColorBrewer)
 # Calculate mean change in minimum temperatures in 2050 in areas where coffee Arabica is grown, by country
 
 # Show total value of this production
-countries <- "'ETH', 'UGA', 'CIV', 'TZA', 'KEN', 'BRD', 'RWA'"
+ISOs <- c("ETH", "UGA", "CIV", "TZA", "KEN", "BRD", "RWA")
+ISOs_string <- paste0("'", paste(ISOs, collapse="', '"), "'")
 
 # Calculate historical temperature trends in each of these countries from this data
 data_prefix <- 'global_cru_ts3_23_'
@@ -19,7 +23,7 @@ data_suffix <- '_19850101_20141231_trend_decadal'
 datasets <- c('tmn', 'tmp', 'tmx')
 hist_temps <- foreach (dataset=datasets, .combine=rbind) %do% {
     hist_q <- paste0('SELECT name_engli, min(val), avg(val), max(val)
-    FROM (select * from gadm28_adm0 WHERE iso IN (', countries, ')) AS countries
+    FROM (select * from gadm28_adm0 WHERE iso IN (', ISOs_string, ')) AS countries
     , (
       SELECT val, geom AS the_geom_webmercator
         FROM (
@@ -39,30 +43,54 @@ hist_temps <- foreach (dataset=datasets, .combine=rbind) %do% {
 hist_temps$period <- '1985-2015'
 
 # Calculate future temperature trends in each of these countries from this data
-fut_q <- paste0('SELECT name_engli, 
-(ST_SummaryStats(ST_SetBandNoDataValue(ST_Union(ST_Clip(the_raster_webmercator, the_geom_webmercator, -9999)), -9999), TRUE)).*
-FROM tasmin_rcp85_absdiff_1986_2005_vs_2040_2059_annual, (select * from gadm28_adm0 WHERE iso IN (', countries, ')) AS countries
-GROUP BY name_engli
-ORDER BY name_engli')
-fut_temps <- cdb_sql_call(fut_q)
-fut_temps <- fut_temps$row
+fut_temps_allmodels <- brick('diff_tasmin_rcp85_2040-2059.tif')
+proj4string(fut_temps_allmodels) <- '+init=epsg:4326'
+
+countries <- readOGR('O:/Data/Global/GADM', 'gadm28_adm0')
+countries <- countries[countries$ISO %in% ISOs, ]
+countries$NAME_ENGLI <- as.character(countries$NAME_ENGLI)
+
+fut_temps <- foreach (n=1:nrow(countries), .combine=rbind) %do% {
+    this_country <- countries[n, ]
+    temp <- mask(crop(fut_temps_allmodels, this_country), this_country)
+
+    # Calculate multimodel mean
+    mean_chgs <- apply(getValues(temp), 2, mean, na.rm=TRUE)
+
+    # Calculate min and max projections
+    data.frame(name_engli=this_country$NAME_ENGLI,
+               avg=mean(mean_chgs),
+               min=min(mean_chgs),
+               max=max(mean_chgs))
+}
+
 # Convert from change over next 35 years to change per decade
-fut_temps$min <-fut_temps$min / 3.5
-fut_temps$mean <-fut_temps$mean / 3.5
-fut_temps$max <-fut_temps$max / 3.5
-fut_temps <- select(fut_temps, -sum, -stddev, -count, avg=mean)
+fut_temps$min <- fut_temps$min / 3.5
+fut_temps$avg <- fut_temps$avg / 3.5
+fut_temps$max <- fut_temps$max / 3.5
 fut_temps$var <- 'tmn'
 fut_temps$period <- '2040-2060'
+# Fix encoding so Cote d'Ivoire appears correctly
+fut_temps$name_engli <- iconv(fut_temps$name_engli, "UTF-8")
+fut_temps$name_engli <- enc2utf8(as.character(fut_temps$name_engli))
 
 temps <- full_join(hist_temps, fut_temps)
 
-temps$name_engli[temps$name_engli == 'Democratic Republic of the Congo'] <- 'DRC'
+# Don't plot the errorbars on the historical estimates as they are the observed 
+# data, without model uncertainty (though of course with other errors 
+# sources...)
+temps$min[temps$period != '2040-2060'] <- NA
+temps$max[temps$period != '2040-2060'] <- NA
 
-p <- plot_ly(filter(temps, var == 'tmn'),
-                 x = ~name_engli, y = ~avg, color = ~period, colors='Accent') %>%
-    add_markers(error_y = ~list(type="data", symmetric=FALSE, 
-                                arrayminus=avg-min, array=max-avg)) %>%
-    config(showLink = F) %>%
-    config(displayModeBar = F)
-p <- layout(p, xaxis=list(title=""), yaxis = list(title = "Change in minimum temperature (C)"))
+temps$period[temps$period != '2040-2060'] <- "Historical"
+temps$period[temps$period == '2040-2060'] <- "Future"
+
+temps <- filter(temps, var == 'tmn')
+
+p <- plot_ly(x = ~name_engli, y = ~avg, color = ~period, colors = 'Accent') %>%
+    add_markers(data = filter(temps, period == 'Historical')) %>%
+    add_markers(error_y  =  ~list(type = "data", symmetric = FALSE, arrayminus = avg-min, array = max-avg), data = filter(temps, period == 'Future')) %>%
+    config(showLink = FALSE) %>%
+    config(displayModeBar = FALSE)
+p <- layout(p, xaxis=list(title=""), yaxis = list(title = "Rate of change in minimum temperature (C)"))
 plotly_POST(p, filename = "african-coffee-mintemp-trends")
